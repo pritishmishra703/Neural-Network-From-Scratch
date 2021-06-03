40e06d7e2a7c2fe4bf35dcb0fa9ef84d86183a70
Pritish Mishra <pritishjan@gmail.com>
2021-06-03 17:11:32 +0530

    removed __init__ function from activation_functions.py and instead given it in forward function

--- activation_functions.py
+++ activation_functions.py
@@ -2,35 +2,25 @@ import numpy as np
 
 class elu:
 
-    def __init__(self, x, alpha=1.0):
-        self.x = x
-        self.alpha = alpha
-
-    def forward(self):
+    def forward(self, x, alpha):
         output_matrix = []
-        for i in np.nditer(self.x):
+        for i in np.nditer(x):
             if i > 0:
                 output_matrix.append(i)
             else:
-                output_matrix.append(self.alpha*(np.exp(i) - 1))
-        return np.array(output_matrix).reshape(self.x.shape).astype(np.float32)
+                output_matrix.append(alpha*(np.exp(i) - 1))
+        return np.array(output_matrix).reshape(x.shape).astype(np.float32)
 
 class exponential:
 
-    def __init__(self, x):
-        self.x = x
-
-    def forward(self):
-        return np.exp(self.x).astype(np.float32)
+    def forward(self, x):
+        return np.exp(x).astype(np.float32)
 
 class hard_sigmoid:
 
-    def __init__(self, x):
-        self.x = x
-
     def forward(self):
         output = []
-        for i in np.nditer(self.x):
+        for i in np.nditer(x):
             if i < -2.5:
                 output.append(0)
             elif i > 2.5:
@@ -38,73 +28,49 @@ class hard_sigmoid:
             elif -2.5 <= i <= 2.5:
                 output.append(0.2*i + 0.5)
 
-        return np.array(output).reshape(self.x.shape).astype(np.float32)
+        return np.array(output).reshape(x.shape).astype(np.float32)
 
 class relu:
 
-    def __init__(self, x):
-        self.x = x
-
-    def forward(self):
-        return np.maximum(0, self.x).astype(np.float32)
+    def forward(self, x):
+        return np.maximum(0, x).astype(np.float32)
 
 class selu:
 
-    def __init__(self, x):
-        self.x = x
-
-    def forward(self):
+    def forward(self, x):
         alpha=1.67326324
         scale=1.05070098
-        return scale*elu(self.x, alpha)
+        return scale*elu(x, alpha).astype(np.float32)
 
 class sigmoid:
 
-    def __init__(self, x):
-        self.x = x
-
-    def forward(self):
-        return 1/(1 + np.exp(-self.x))
+    def forward(self, x):
+        return 1/(1 + np.exp(-x)).astype(np.float32)
 
 class softmax:
-
-    def __init__(self, x):
-        self.x = x
     
-    def forward(self):
-        exp_values = np.exp(self.x - np.max(self.x, keepdims=True, axis=1))
+    def forward(self, x):
+        exp_values = np.exp(x - np.max(x, keepdims=True, axis=1))
         output = exp_values/np.sum(exp_values, keepdims=True, axis=1)
-        return output
+        return output.astype(np.float32)
 
 class softplus:
 
-    def __init__(self, x):
-        self.x = x
-
-    def forward(self):
-        return np.log(np.exp(self.x) + 1)
+    def forward(self, x):
+        return np.log(np.exp(x) + 1).astype(np.float32)
 
 class swish:
-
-    def __init__(self, x):
-        self.x = x
     
-    def forward(self):
-        return self.x * sigmoid(self.x)
+    def forward(self, x):
+        return x * sigmoid(x).astype(np.float32)
 
 class tanh:
 
-    def __init__(self, x):
-        self.x = x
-
-    def forward(self):
-        return (np.exp(self.x) - np.exp(-self.x))/(np.exp(self.x) + np.exp(-self.x))
+    def forward(self, x):
+        return (np.exp(x) - np.exp(-x)) \
+            /(np.exp(x) + np.exp(-x)).astype(np.float32)
 
 class leaky_relu:
 
-    def __init__(self, x, alpha=0.3):
-        self.x = x
-        self.alpha = alpha
-
-    def forward(self):
-        return np.maximum(self.alpha*self.x, self.x)
+    def forward(self, x, alpha=0.3):
+        return np.maximum(alpha*x, x).astype(np.float32)
